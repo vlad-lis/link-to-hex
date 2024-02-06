585d2896ddd67d8756c405e87bb0bada92633f8b
vladislav lis <lis.vladislav@gmail.com>
2024-02-06 18:55:44 +0300

    feat: add logo click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,28 @@
 import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 import HeaderNav from '../HeaderNav/HeaderNav';
 
 const Header = (): ReactElement => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = (): void => {
+    navigate('/squeeze');
+  };
+
   return (
     <header className={styles.header}>
-      <div className={styles['header__logo-wrapper']}>
+      <div
+        className={styles['header__logo-wrapper']}
+        onClick={handleLogoClick}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            handleLogoClick();
+          }
+        }}
+        tabIndex={0}
+        role='button'
+      >
         <div className={styles.header__logo} />
         <p className={styles['header__logo-title']}>Link Hexer</p>
       </div>
