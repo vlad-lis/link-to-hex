c50de247455b8540fc9964a1ac6542117270bf5f
vladislav lis <lis.vladislav@gmail.com>
2024-02-06 15:50:56 +0300

    chore: switch to hash router for ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import RoutesComponent from './components/RoutesComponent/RoutesComponent';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <RoutesComponent />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <RoutesComponent />
+    </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import App from './App';
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
